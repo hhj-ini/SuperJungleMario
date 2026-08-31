86db58551e6e7a8f6cfd0af6266dd731620f46d8
taehun01 <kth010910@naver.com>
2026-09-01 00:21:29 +0900

    사각형 크기 0.5로 증가

--- SuperJungleMario/Cube.h
+++ SuperJungleMario/Cube.h
@@ -1,10 +1,10 @@
-FVertexSimple v0 = { -0.2f, -0.2f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f };
-FVertexSimple v1 = { 0.2f, -0.2f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f };
-FVertexSimple v2 = { -0.2f, 0.2f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f };
-FVertexSimple v3 = { 0.2f, 0.2f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f };
+FVertexSimple v0 = { -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f };
+FVertexSimple v1 = { 0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f };
+FVertexSimple v2 = { -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f };
+FVertexSimple v3 = { 0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f };
 
 FVertexSimple cube_vertices[] =
 {
-    v0, v1, v2,
+    v0, v2, v1,
     v1, v2, v3
 };
