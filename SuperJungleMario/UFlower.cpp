abb8066ffa1bf7105cbb19a1d8ed6d6df540e610
이경호 <no_rudgh@naver.com>
2026-09-02 11:26:24 +0900

    Flower 기능추가. main테스트도 추가
    Flower 기능추가. main테스트도 추가

---
+++ SuperJungleMario/UFlower.cpp
@@ -0,0 +1,71 @@
+#include "UFlower.h"
+#include "ResourceManager.h"
+
+
+void UFlower::Render(URenderer& renderer, ID3D11Buffer* pBuffer, UINT num) {
+	if (mrState == FlowerState::ENABLE || mrState == FlowerState::ANIMATING)
+	{	// 버섯이 애니메이션 중이거나, 활성화 상태일 때만 렌더링
+
+		/*renderer.UpdateConstantBuffer(Location, Radius);
+		renderer.RenderPrimitive(pBuffer, num);*/
+
+		// 렌더하기 전에 텍스쳐 바인딩
+		if (!TextureSRVPtr[0])
+		{
+			TextureSRVPtr[0] = ResourceManager::GetInstance().GetSRV(L"Resource\\Flower.png", &renderer);
+		}
+		renderer.PrepareShaderResource(TextureSRVPtr[0]);
+
+		DirectX::XMMATRIX world = DirectX::XMMatrixScaling(width, height, 1.0f) * DirectX::XMMatrixTranslation(Location.x, Location.y, Location.z);
+		renderer.UpdateConstantBuffer(world, renderer.ViewMatrix);
+
+		renderer.RenderPrimitive(pBuffer, num);
+	}
+}
+
+bool UFlower::CollisionCheck(UPrimitive* other) {
+	// 가로가 겹치는지 확인
+	float sumHalfWidth = (width / 2.0f) + (other->width / 2.0f);
+	float xdistance = std::fabs(Location.x - other->Location.x);
+	float overlapX = sumHalfWidth - xdistance;
+
+	// 세로가 겹치는지 확인
+	float sumHalfHeight = (height / 2.0f) + (other->height / 2.0f);
+	float ydistance = std::fabs(Location.y - other->Location.y);
+	float overlapY = sumHalfHeight - ydistance;
+
+	// 충돌
+	if (overlapX > 0 && overlapY > 0) {
+
+		switch (other->ObjectType)
+		{
+		case EObjectType::PLAYER: 
+			mrState = FlowerState::DESTROYED; // 플레이어와 충돌 시 꽃을 DESTROYED 상태로 변경
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	return false;
+}
+
+void UFlower::FlowFlower() {
+	Velocity.y = 0.01f; // 꽃이 위로 올라가는 속도 설정
+	mrState = FlowerState::ANIMATING; // 꽃 상태를 ANIMATING으로 변경
+}
+
+void UFlower::Move() {
+	if (mrState == FlowerState::ANIMATING) {
+		Location.y += 0.01f; // 꽃이 위로 올라감
+		if (Location.y >= UFlower::InitHeight+height) {
+			Velocity.y = 0.0f;
+			mrState = FlowerState::ENABLE; // 꽃이 다 올라오면 ENABLE 상태로 변경
+		}
+	}
+	return;
+}
+
+
+
---
+++ SuperJungleMario/UFlower.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "UBall.h"
+
+/*
+Flower 작동구조
+1. 마리오가 박스를 때림
+2. 박스가 꽃 생성 -> FlowFlower() 호출
+3. 꽃 떠오름 
+4. 꽃 다 떠오름
+5. 꽃 활성화
+6. 마리오가 꽃 먹음
+7. 꽃 사라짐
+*/
+class UFlower :
+    public UBall
+{
+public:
+
+    enum class FlowerState
+    {
+        WAITING, // 블럭속에서 기다리고있음
+		ANIMATING, // 블럭에서 올라오는 애니메이션 중
+		ENABLE, // 활성화 상태, 플레이어가 먹을 수 있음
+		DESTROYED // 플레이어가 먹어서 사라짐
+    };
+
+	UFlower(float x, float y, float w, float h)
+	{
+		ObjectType = EObjectType::FLOWER;
+		Location.x = x;
+		Location.y = y;
+		width = w * scaleMod;
+		height = h * scaleMod;
+		bisMove = false;
+        InitHeight= y;
+	}
+    UFlower() {}
+    virtual void Render(URenderer& renderer, ID3D11Buffer* pBuffer, UINT num) override;
+    virtual bool CollisionCheck(UPrimitive* other)override;
+
+    virtual void Move() override;
+    bool IsFlowerDestroyed() const { return mrState == FlowerState::DESTROYED; }
+
+    void SetEnable();
+
+    void SetAnimation();
+
+    void FlowFlower();
+    // void UpdateVelocity(bool bGravity) override;
+
+private:
+    FlowerState mrState = FlowerState::WAITING;
+    float InitHeight;
+};
+
