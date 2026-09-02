2bc7554d583b876d34761e663335b6d821b5f006
권혜원 <130565897+12e0hw@users.noreply.github.com>
2026-09-02 21:22:18 +0900

    Player 속도 조절

--- SuperJungleMario/UPlayer.cpp
+++ SuperJungleMario/UPlayer.cpp
@@ -283,13 +283,13 @@ void UPlayer::Move()
 
 		if (GetAsyncKeyState(VK_LEFT) & 0x8000)
 		{
-			Velocity.x = -0.01f;
+			Velocity.x = -0.02f;
 			bFacingLeft = true;
 			// bIsGrounded = false;
 		}
 		else if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
 		{
-			Velocity.x = 0.01f;
+			Velocity.x = 0.02f;
 			bFacingLeft = false;
 			// bIsGrounded = false;
 		}		
