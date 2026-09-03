37d0ed14421f413c806e83932290d7eea1b6511e
hhz <itree1030@gmail.com>
2026-09-03 13:11:02 +0900

    게임 플레이 사운드 재시작에서도 재생되게 함

--- SuperJungleMario/main.cpp
+++ SuperJungleMario/main.cpp
@@ -429,7 +429,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			player->Reset(); // 플레이어 초기화
 			camera.Reset(); // 카메라 초기화
 
-			//UGameLogic::GetInstance().setStarted();
+			
 		}
 		if (player && (player->IsPlayerDead() || player->Location.y < -2.5f))  // 플레이어가 죽었을때 되돌아가는 부분
 		{
